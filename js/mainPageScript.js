4666a192f66b8d4237d6c158734ff720891f1376
ImJamie <jamie@929design.co.uk>
2026-01-18 17:37:22 +0000

    Up to date upload of files

--- js/mainPageScript.js
+++ js/mainPageScript.js
@@ -13,4 +13,44 @@ tabButtons.forEach(button => {
     button.classList.add('active');
     document.getElementById(tabId).classList.add('active');
   });
+
+  document.querySelectorAll('.mobile-offset-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      if (window.innerWidth <= 768) {
+        e.preventDefault();
+
+        const id = this.getAttribute('href').substring(1);
+        const target = document.getElementById(id);
+        if (!target) return;
+
+        const nav = document.querySelector('nav');
+        const offset = nav ? nav.offsetHeight : 0; // adjust as needed
+        const y =
+          target.getBoundingClientRect().top +
+          window.pageYOffset -
+          offset + 20;
+
+        window.scrollTo({
+          top: y,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    if (window.innerWidth > 800) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (!target) return;
+
+        target.scrollIntoView({
+          behavior: 'smooth',
+          // block: 'start'
+        });
+      });
+    }
+  });
 });
